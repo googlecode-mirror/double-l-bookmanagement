8e822b5869e0fc6b08a0372e16e00e66fe0e93e8
longtai.tw <longtai.tw@gmail.com>
2012-11-18 16:14:54 +0000

    新增 是否可以外借選項

--- cakephp/app/Controller/Component/FormfuncComponent.php
+++ cakephp/app/Controller/Component/FormfuncComponent.php
@@ -12,6 +12,9 @@
 		public function book_status() {
 			return array(0=>"購買中",1=>"在庫",2=>"借出",3=>"已歸還",4=>"整理中",5=>"運送中");
 		}
+		public function is_lends(){
+			return array("Y"=>"Y","N"=>"N");
+		}
 		
 		public function person_gender() {
 			return array(0=>"女",1=>"男");

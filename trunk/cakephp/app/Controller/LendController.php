541ddfc3db1bce95d4a71c533299fffc191988d0
leokao.tw@gmail.com <leokao.tw@gmail.com@45bca3c5-b13b-f0dc-51dd-38a452a9798e>
2012-11-23 09:24:31 +0000

    Add jquery datepicker

--- trunk/cakephp/app/Controller/LendController.php
+++ trunk/cakephp/app/Controller/LendController.php
@@ -2,11 +2,17 @@
 class LendController extends AppController {
 	public $uses = array('Book_Cate', 'Book', 'Book_Instance','Book_Publisher', 'Person_Level', 'Lend_Record');
     public $helpers = array('Html', 'Form', 'Session');
-    public $components = array('Session', 'Formfunc');
+    public $components = array('Session', 'Formfunc', 'Leadfunc');
 
     public function index(){
 		$lend_records = $this->Lend_Record->find('all');
     	$this->set('lend_records', $lend_records);  	 
-    }    
+    } 
+
+	public function lead_operation() {
+		if (!empty($this->data)) {
+		
+		}
+	}
 }
 ?>

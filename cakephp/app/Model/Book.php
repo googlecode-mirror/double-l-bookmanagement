d8cf03cafa2a6a60e77d6633d0575ef57a2e6577
longtai.tw <longtai.tw@gmail.com>
2013-03-27 13:55:09 +0000

    1. 薪增 Lexile_level = 0, 視為中文書 2. cate_id = 0, book_instnce_id 編碼為CH 3. 修正 lexile_level 100=>100極

--- cakephp/app/Model/Book.php
+++ cakephp/app/Model/Book.php
@@ -5,7 +5,7 @@ class Book extends AppModel {
 	public $validate = array(
 		'lexile_level' => array(
 				array('rule'=> 'numeric','message' => 'Lexile級別必須是數字'),
-				array('rule'=> array('between', 0, 1000),'message' => 'Lexile級別必須介於0~1000之間')
+				array('rule'=> array('between', 0, 2000),'message' => 'Lexile級別必須介於0~2000之間')
 		)	
 	);
 	public $hasMany = array(
@@ -22,11 +22,13 @@ class Book extends AppModel {
 	);
 
 	public function beforeSave($options = array()){
-		//var_dump($this->Data);
-		if($this->data['Book']['lexile_level'] > 900) {
+		
+		if($this->data['Book']['lexile_level'] == 0){
+			$this->data['Book']['cate_id'] = 0;
+		} else if($this->data['Book']['lexile_level'] > 900) {
 			$this->data['Book']['cate_id'] = 1000;
 		} else {
-			$this->data['Book']['cate_id'] = (floor($this->data['Book']['lexile_level']/100)+1)*100;
+			$this->data['Book']['cate_id'] = (floor(($this->data['Book']['lexile_level']-1)/100)+1)*100;
 		}
 		return true;
 	}
